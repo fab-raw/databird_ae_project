f0a13914993359d8be23883e1c766740e3f4c35a
fab-raw <143786524+fab-raw@users.noreply.github.com>
2024-11-19 17:30:38 +0100

    feat : suffixe b

--- models/staging/sales_database/stg_sales_database__order.sql
+++ models/staging/sales_database/stg_sales_database__order.sql
@@ -1,10 +1,10 @@
 select
-    order_id_y,
-    user_name as user_id_y,
-    order_status_y,
-    DATETIME(order_date, "Europe/Paris") AS order_created_at_x,
-    DATETIME(order_approved_date, "Europe/Paris") AS order_approved_at_x,
-    DATETIME(pickup_date, "Europe/Paris") AS picked_up_at_x,
-    DATETIME(delivered_date, "Europe/Paris") AS delivered_at_x,
-    DATETIME(estimated_time_delivery, "Europe/Paris") AS estimated_time_delivery_x
+    order_id_b,
+    user_name as user_id_b,
+    order_status_b,
+    DATETIME(order_date, "Europe/Paris") AS order_created_at_b,
+    DATETIME(order_approved_date, "Europe/Paris") AS order_approved_at_b,
+    DATETIME(pickup_date, "Europe/Paris") AS picked_up_at_b,
+    DATETIME(delivered_date, "Europe/Paris") AS delivered_at_b,
+    DATETIME(estimated_time_delivery, "Europe/Paris") AS estimated_time_delivery_b
 from {{ source('sales_database', 'order') }}

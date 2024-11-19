03e438bec4b946ab449bb09330f943caae36ebf1
fab-raw <143786524+fab-raw@users.noreply.github.com>
2024-11-19 17:51:16 +0100

    commit 1

--- models/staging/sales_database/stg_sales_database__order_item.sql
+++ models/staging/sales_database/stg_sales_database__order_item.sql
@@ -1,9 +1,9 @@
-select CONCAT(order_id, '_', product_id) AS order_item_id,
- order_id,
- product_id,
- seller_id,
- DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at,
- price as unit_price,
- shipping_cost,
- (price * quantity) + shipping_cost as total_order_item_amount
+select CONCAT(order_id, '_', product_id) AS order_item_id_x,
+ order_id_x,
+ product_id_x,
+ seller_id_x,
+ DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at_x,
+ price as unit_price_x,
+ shipping_cost_x,
+ (price * quantity) + shipping_cost as total_order_item_amount_x
 from {{ source('sales_database', 'order_item') }}

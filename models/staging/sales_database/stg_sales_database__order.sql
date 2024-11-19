41cc35e611e85b6620ae819becb6188cff8d7963
fab-raw <143786524+fab-raw@users.noreply.github.com>
2024-11-19 17:35:42 +0100

    fix : conflit de a et b

--- models/staging/sales_database/stg_sales_database__order.sql
+++ models/staging/sales_database/stg_sales_database__order.sql
@@ -1,7 +1,7 @@
 select
-    order_id_y,
-    user_name as user_id_a,
-    order_status_a,
+    order_id_b,
+    user_name as user_id_b,
+    order_status_b,
     DATETIME(order_date, "Europe/Paris") AS order_created_at_a,
     DATETIME(order_approved_date, "Europe/Paris") AS order_approved_at_a,
     DATETIME(pickup_date, "Europe/Paris") AS picked_up_at_a,
